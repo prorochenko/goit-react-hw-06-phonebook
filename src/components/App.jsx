a818a5d830f7ee763c98e9603d52628f431d5b66
prorochenko <33101252+prorochenko@users.noreply.github.com>
2022-11-19 22:02:47 +0200

    added redux final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,63 +1,17 @@
-// import { useState, useEffect } from 'react';
 import Contacts from './Contacts/Contacts';
 import Section from './Section/Section';
 import Filter from './Filter/Filter';
 import css from './CommonStyle.module.scss';
 import Form from './Form/Form';
-// import { nanoid } from 'nanoid';
-// import Notiflix from 'notiflix';
+
 import toast, { Toaster } from 'react-hot-toast';
-
-// import { getFilter } from 'redux/filterSlice';
-
-// import { useSelector, useDispatch } from 'react-redux';
+import scss from './Contacts/Contacts.module.scss';
 
 export default function App() {
   const notify = () =>
     toast.success('Good Job! 👏', {
       position: 'top-right',
     });
-
-  // const [contacts, setContacts] = useState(() => {
-  //   return JSON.parse(window.localStorage.getItem('contacts' ?? ''));
-  // });
-
-  // const [filter, setFilter] = useState('');
-
-  // const deleteContact = contactId => {
-  //   setContacts(prevState =>
-  //     prevState.filter(contact => contact.id !== contactId)
-  //   );
-  // };
-
-  // const formSubmitHandler = ({ name, number }) => {
-  //   const contact = {
-  //     id: nanoid(),
-  //     name,
-  //     number,
-  //   };
-
-  //   if (contacts.some(contact => contact.name === name)) {
-  //     Notiflix.Notify.failure(`${name} is already in contacts`);
-  //     return;
-  //   }
-
-  //   setContacts(prevState => [contact, ...prevState]);
-  // };
-
-  // const changeFilter = e => {
-  //   setFilter(e.currentTarget.value);
-  // };
-
-  // const getVisibleContacts = () => {
-  //   return contacts.filter(contact =>
-  //     contact.name.toLowerCase().includes(getFilter)
-  //   );
-  // };
-
-  // useEffect(() => {
-  //   localStorage.setItem('contacts', JSON.stringify(contacts));
-  // }, [contacts]);
 
   return (
     <div className={css.container}>
@@ -68,7 +22,7 @@ export default function App() {
         <Filter />
         <Contacts />
       </Section>
-      <button type="button" onClick={notify}>
+      <button className={scss.list__btn} type="button" onClick={notify}>
         press me
       </button>
       <div>
